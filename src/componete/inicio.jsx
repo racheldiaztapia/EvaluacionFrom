0488534abdcd6483550b0e6df35fb78378cccfff
racheldiaztapia <139297503+racheldiaztapia@users.noreply.github.com>
2023-07-22 00:47:24 -0400

    Update inicio.jsx

--- src/componete/inicio.jsx
+++ src/componete/inicio.jsx
@@ -5,9 +5,27 @@ const inicio = () => {
   return (
     <div>
       <h1>Esta es la página de inicio</h1>
-      <Link to="sobre-nosotros">Haz clic para ver la página sobre nosotros</Link><br/>
-      <Link to="contacto">Haz clic para ver la página de contacto</Link><br/>
-      <Link to="descrip">Haz clic para ver la página de descrip</Link>
+      <div id="carouselExample" class="carousel slide">
+      <div class="carousel-inner">
+      <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="..."/>
+     </div>
+     <div class="carousel-item">
+         <img src="..." class="d-block w-100" alt="..."/>
+     </div>
+      <div class="carousel-item">
+         <img src="..." class="d-block w-100" alt="..."/>
+      </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+      </button>
+      </div>
     </div>
   )
 }
