8110eac9e72cd9b8ec3ad8cc535bdf755636b047
racheldiaztapia <139297503+racheldiaztapia@users.noreply.github.com>
2023-07-22 00:32:33 -0400

    Update navbar.jsx

--- src/componete/navbar.jsx
+++ src/componete/navbar.jsx
@@ -5,7 +5,7 @@ const navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Bienvenidos</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
